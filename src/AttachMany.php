bf88b5b9980e25f864f800d1efd18ae230e6b216
Mikhail Buravlev <mb@joom.com>
2019-12-08 23:26:32 +0300

    fix update when field not provided

--- src/AttachMany.php
+++ src/AttachMany.php
@@ -44,9 +44,11 @@ class AttachMany extends Field
         $this->fillUsing(function($request, $model, $attribute, $requestAttribute) use($resource) {
             if(is_subclass_of($model, 'Illuminate\Database\Eloquent\Model')) {
                 $model::saved(function($model) use($attribute, $request) {
-                    $model->$attribute()->sync(
-                        json_decode($request->$attribute, true)
-                    );
+                    if ($request->$attribute != NULL) {
+                        $model->$attribute()->sync(
+                            json_decode($request->$attribute, true)
+                        );
+                    }
                 });
 
                 unset($request->$attribute);
